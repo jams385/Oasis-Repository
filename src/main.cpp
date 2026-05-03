9b29dc6c81e5206b240dfb713e1eae5759302ffd
Jamil <jamilpalma704@gmail.com>
2026-05-04 00:14:33 +0800

    Updated SFML project setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,22 @@
-#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
 
 int main()
 {
-	sf::RenderWindow window( sf::VideoMode( { 200, 200 } ), "SFML works!" );
-	sf::CircleShape shape( 100.f );
-	shape.setFillColor( sf::Color::Green );
+	//initializes the window
+    sf::Window window(sf::VideoMode(800, 600), "My window");
 
-	while ( window.isOpen() )
-	{
-		while ( const std::optional event = window.pollEvent() )
-		{
-			if ( event->is<sf::Event::Closed>() )
-				window.close();
-		}
+    // run the program as long as the window is open
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+    }
 
-		window.clear();
-		window.draw( shape );
-		window.display();
-	}
+    return 0;
 }
