703215d2c44fea8574bf4653bfa432997b251d2f
Jamil <jamilpalma704@gmail.com>
2026-05-29 06:28:54 +0800

    finished pathfinding

--- include/Enemy.h
+++ include/Enemy.h
@@ -1,6 +1,9 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include "AnimatedSprite.h"
+#include <vector>
+
+class Map;
 
 enum class EnemyType {
     DustMummy,
@@ -13,7 +16,7 @@ class Enemy {
 public:
     Enemy(EnemyType type, sf::Vector2f startPos, int splitGeneration = 0);
 
-    void update(float dt, sf::Vector2f target);
+    void update(float dt, sf::Vector2f target, const Map& map);
     void draw(sf::RenderWindow& window);
 
     void takeDamage(float amount);
@@ -46,6 +49,12 @@ private:
     
     float           slowTimer    = 0.f;
     float           slowFactor   = 1.f;
+
+    std::vector<sf::Vector2f> waypoints_;
+    int                       waypointIdx_ = 0;
+    float                     pathAge_     = 999.f;  // forces path on first update
+    sf::Vector2f              lastTarget_;
+    static constexpr float    PATH_RECOMPUTE = 1.5f;
 
     sf::CircleShape shape;
     AnimatedSprite  animSprite;
